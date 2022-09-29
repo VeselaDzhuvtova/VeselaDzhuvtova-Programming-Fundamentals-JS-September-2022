17d8db668396d6d10d19f426d5098696bbaadf5d
VeselaDzhuvtova <108488627+VeselaDzhuvtova@users.noreply.github.com>
2022-09-29 20:47:20 +0300

    StudentInformation
    You will be given 3 parameters – student name (string), age (number), and average grade (number). Your task is to print all the info about the student in the following format:
    `Name: {student name}, Age: {student age}, Grade: {student grade}`

---
+++ Basic Syntax, Conditional Statements and Loops/StudentInformation/introduce.js
@@ -0,0 +1,5 @@
+function introduse(studentName, studentAge, studentGrade) {
+    let introduction = `Name: ${studentName}, Age: ${studentAge}, Grade: ${studentGrade.toFixed(2)}`;
+    console.log(introduction)
+}
+introduse('Jonh',15, 5.54678);
